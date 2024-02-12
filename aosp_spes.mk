336b48a8da2d2af40f670eee6276c5298656be81
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-02-12 09:48:26 +0200

    CraftRomOS BringUp

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -1,12 +1,24 @@
-# Inherit from framework configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from spes device configuration
+# Inherit from common CraftRomOS configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# CraftRomOS specific flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USES_AOSP_RECOVERY := true
+
+# CraftRom specific flags
+CUSTOM_MAINTAINER  := GuidixX
 
 # Device identifier
 PRODUCT_NAME := aosp_spes
@@ -16,7 +28,3 @@ PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Target
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
